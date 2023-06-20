caa268a9d23771ab2669d457ab05b5aa2e829124
LidiaMartinezMena <122877225+LidiaMartinezMena@users.noreply.github.com>
2023-06-20 11:31:35 +0200

    Add files via upload

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -20,11 +20,13 @@ const agregarTarea = async (req, res) => {
 
     try {
         const tareaAlmacenada = await Tarea.create(req.body);
+        //Almacenar el ID: 
+        existeProyecto.tareas.push(tareaAlmacenada._id);
+        await existeProyecto.save();
         res.json(tareaAlmacenada);
     } catch (error) {
         console.log(error);
     }
-    console.log(existeProyecto);
 };
 
 const obtenerTarea = async (req, res) => {
